c14a98102636ee230d6e014b26166bfd62625e9e
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-14 11:47:36 +0200

    create CartRepository and ICartRepository.

---
+++ FreshHub_ASP_NET/FreshHub_BE/Services/CartRepository/CartRepository.cs
@@ -0,0 +1,55 @@
+﻿using FreshHub_BE.Data;
+using FreshHub_BE.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreshHub_BE.Services.CartRepository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly AppDbContext context;
+
+        public CartRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<CartItem> AddItem(int userId, CartItem item)
+        {
+           var user = await context.Users
+                .Include(x=>x.Carts)
+                .ThenInclude(x=>x.CartItems)
+                .FirstAsync(x=>x.Id == userId);
+
+            user.Carts.First().CartItems.Add(item);
+            
+            await context.SaveChangesAsync();
+
+            return item;           
+        }
+
+        public async Task DeleteItem(CartItem item)
+        {
+            context.Entry(item).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+            
+        }
+
+        public async Task<Cart> GetCart(int Id)
+        {
+            if (await context.Users.AnyAsync(x=>x.Id == Id))
+            {
+                return await context.Carts
+                    .Include(x=>x.CartItems)
+                    .Where(x => x.UserId == Id)
+                    .SingleAsync();
+            }
+            throw new System.Exception("Invalid User..");
+        }
+
+        public async Task<CartItem> UpdateItem(CartItem item)
+        {
+            context.Entry(item).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
---
+++ FreshHub_ASP_NET/FreshHub_BE/Services/CartRepository/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using FreshHub_BE.Data.Entities;
+
+namespace FreshHub_BE.Services.CartRepository
+{
+    public interface ICartRepository
+    {
+        Task<Cart> GetCart(int Id);
+        Task<CartItem> AddItem(int Id, CartItem item);
+        Task<CartItem> UpdateItem(CartItem item);
+        Task DeleteItem(CartItem item);
+
+    }
+}
