9c6565412686f2f8bc96d699a662b9291c4696ff
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-14 10:57:31 +0200

    create OrderModel, OrderResultModel.

---
+++ FreshHub_ASP_NET/FreshHub_BE/Models/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using FreshHub_BE.Data.Entities;
+
+namespace FreshHub_BE.Models
+{
+    public class OrderModel
+    {       
+        public TimeOnly OrderTimeOnly { get; set; }
+        public DateOnly OrderDateOnly { get; set; }
+        public string Recipient { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Comment { get; set; }
+        public int NumberPerson { get; set; }
+        public bool Call { get; set; }
+        public string Payment { get; set; }        
+        public bool PaymentStatus { get; set; }
+        public string StreetHouse { get; set; }
+        public string Flat { get; set; }
+        public string Floor { get; set; }
+    }
+}
---
+++ FreshHub_ASP_NET/FreshHub_BE/Models/OrderResultModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FreshHub_BE.Models
+{
+    public class OrderResultModel
+    {
+        public int Id { get; set; }
+        public decimal Summ { get; set; }
+    }
+}
