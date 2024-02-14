8d7fa1124beb5119ecb8d6f09406ccac081630f0
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-14 11:49:06 +0200

    create OrderModelValidation.

---
+++ FreshHub_ASP_NET/FreshHub_BE/Validation/OrderModelValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FreshHub_BE.Models;
+
+namespace FreshHub_BE.Validation
+{
+    public class OrderModelValidation: AbstractValidator<OrderModel>
+    {
+        public OrderModelValidation()
+        {
+            RuleFor(o => o.PhoneNumber)
+                .NotEmpty().WithErrorCode("Empty.")
+                .Length(12).WithErrorCode("Length.");
+            //RuleFor(o => o)
+        }
+    }
+}
