1c94f4cc5534b5e42242191fab15d137dea8966c
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-14 10:56:26 +0200

    add changes for AppDbContext, Seed.

--- FreshHub_ASP_NET/FreshHub_BE/Data/AppDbContext.cs
+++ FreshHub_ASP_NET/FreshHub_BE/Data/AppDbContext.cs
@@ -1,25 +1,60 @@
 ﻿using FreshHub_BE.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FreshHub_BE.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext<User, 
+                               Role, int, IdentityUserClaim<int>, 
+                               UserRole, IdentityUserLogin<int>, 
+                               IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public AppDbContext(DbContextOptions options): base (options)
         {
 
-        }
-        public DbSet<User> Users { get; set; }
+        }        
         public DbSet<Product> Products { get; set; }
         public DbSet<OrderDatail> OrderDatails { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem>CartItems { get; set; }
+        public DbSet<DeliveryAddress> DeliveryAddresses { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
 
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<OrderDatail>().HasKey(x => new { x.OrderId, x.ProductId }); //Fluent API
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderDatail>().HasKey(x => new { x.OrderId, x.ProductId });
+           
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(u => u.UserId)
+                .IsRequired();
+            modelBuilder.Entity<Role>()
+                .HasMany(r => r.UserRoles)
+                .WithOne(r => r.Role)
+                .HasForeignKey(r => r.RoleId)
+                .IsRequired();
+            modelBuilder.Entity<OrderStatus>().HasData(new OrderStatus
+            {
+                Id = 1,
+                Name = "In progress"
+            });
+            modelBuilder.Entity<OrderStatus>().HasData(new OrderStatus
+            {
+                Id = 2,
+                Name = "Done"
+            });
+            modelBuilder.Entity<OrderStatus>().HasData(new OrderStatus
+            {
+                Id = 3,
+                Name = "Rejected"
+            });
+
         }
     }
 }
--- FreshHub_ASP_NET/FreshHub_BE/Data/Seed.cs
+++ FreshHub_ASP_NET/FreshHub_BE/Data/Seed.cs
@@ -1,4 +1,5 @@
 ﻿using FreshHub_BE.Data.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace FreshHub_BE.Data
@@ -15,6 +16,43 @@ namespace FreshHub_BE.Data
             appDbContext.Categories.AddRange(new Category { Name = "Burgers" }, new Category {Name = "Pizza" }, new Category { Name = "Salads" }, new Category { Name = "Desserts" });
             await appDbContext.SaveChangesAsync();
         }
+        public static async Task SeedRole(RoleManager<Role> roleManager)
+        {
+            
+            if (await roleManager.Roles.AnyAsync())
+            {
+                return;
+            }
+            var roles = new List<Role> 
+            {
+                new Role { Name = "User"},
+                new Role { Name = "Admin"},
+                new Role { Name = "Moderator"}
+            };
+            foreach (var role in roles) 
+            {
+                await roleManager.CreateAsync(role);                   
+            }            
+        }
+
+        public static async Task SeedUsers(UserManager<User> userManager)
+        {
+            var users = userManager.Users.ToList();
+            if (await userManager.Users.AnyAsync())
+            {
+                return;
+            }
+            var admin = new User() 
+            {
+              FirstName = "Admin",
+              LastName = "Admin",
+              PhoneNumber = "123456789012",
+              UserName = "Admin"
+            };
+            var resUser = await userManager.CreateAsync(admin, "Pa$$w0rd");
+            var resRole = await userManager.AddToRolesAsync(admin, new[] { "Admin","Moderator",});
+        }
+
 
     }
 }
