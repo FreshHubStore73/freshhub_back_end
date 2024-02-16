7c235ce68c7b6d7fecd4dad8ab2fe0b258df3813
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-16 11:52:54 +0200

    Added changes to the OrderModel

--- FreshHub_ASP_NET/FreshHub_BE/Models/OrderModel.cs
+++ FreshHub_ASP_NET/FreshHub_BE/Models/OrderModel.cs
@@ -4,17 +4,19 @@ namespace FreshHub_BE.Models
 {
     public class OrderModel
     {       
-        public TimeOnly OrderTimeOnly { get; set; }
-        public DateOnly OrderDateOnly { get; set; }
+        public TimeOnly? OrderTimeOnly { get; set; }
+        public DateOnly? OrderDateOnly { get; set; }
         public string Recipient { get; set; }
         public string PhoneNumber { get; set; }
         public string Comment { get; set; }
         public int NumberPerson { get; set; }
         public bool Call { get; set; }
-        public string Payment { get; set; }        
+        public string Payment { get; set; }    
+        public decimal CashSum { get; set; }
         public bool PaymentStatus { get; set; }
         public string StreetHouse { get; set; }
         public string Flat { get; set; }
         public string Floor { get; set; }
+        public int Id { get; set; }
     }
 }
