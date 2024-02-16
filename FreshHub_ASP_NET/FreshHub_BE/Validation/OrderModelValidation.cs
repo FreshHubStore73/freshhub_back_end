f2f1e7d3badd62b28e712f85bc951bb89f76d016
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-16 11:54:02 +0200

    Added changes to the OrderModelValidation

--- FreshHub_ASP_NET/FreshHub_BE/Validation/OrderModelValidation.cs
+++ FreshHub_ASP_NET/FreshHub_BE/Validation/OrderModelValidation.cs
@@ -3,14 +3,27 @@ using FreshHub_BE.Models;
 
 namespace FreshHub_BE.Validation
 {
-    public class OrderModelValidation: AbstractValidator<OrderModel>
+    public class OrderModelValidation : AbstractValidator<OrderModel>
     {
         public OrderModelValidation()
         {
             RuleFor(o => o.PhoneNumber)
                 .NotEmpty().WithErrorCode("Empty.")
                 .Length(12).WithErrorCode("Length.");
-            //RuleFor(o => o)
+            RuleFor(o => o.Flat)
+                .NotEmpty().WithErrorCode("Empty.");
+            RuleFor(o => o.Floor)
+                .NotEmpty().WithErrorCode("Empty.");
+            RuleFor(o => o.StreetHouse)
+                .NotEmpty().WithErrorCode("Empty.");
+            RuleFor(o => o.Payment)
+                .NotEmpty().WithErrorCode("Empty.");
+            RuleFor(o => o.NumberPerson)
+                .GreaterThanOrEqualTo(1).WithErrorCode("Not zero.");
+            RuleFor(o => o.Recipient)
+                .NotEmpty().WithErrorCode("Empty.")
+                .MinimumLength(3).WithErrorCode("Length.");
+
         }
     }
 }
