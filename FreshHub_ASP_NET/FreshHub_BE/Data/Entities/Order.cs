4a5f30b0be02cfb0c50837f4b85e3aa2325850e1
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2024-02-16 11:52:14 +0200

    Added changes to the Order and OrderDetail

--- FreshHub_ASP_NET/FreshHub_BE/Data/Entities/Order.cs
+++ FreshHub_ASP_NET/FreshHub_BE/Data/Entities/Order.cs
@@ -6,17 +6,19 @@
         public int UserId { get; set; }
         public string Recipient { get; set; }
         public string PhoneNumber { get; set; }
-        public TimeOnly OrderTimeOnly { get; set; }
-        public DateOnly OrderDateOnly { get; set; }
+        public TimeOnly? OrderTimeOnly { get; set; }
+        public DateOnly? OrderDateOnly { get; set; }
         public string Comment { get; set; }
         public int NumberPerson { get; set; }
         public bool Call { get; set; }
         public string Payment { get; set; }
+        public decimal CashSum { get; set; }
         public int DeliveryAddressId { get; set; }
         public int OrderStatusId { get; set; } = 1; 
         public bool PaymentStatus { get; set; }
         public User User { get; set; }
         public OrderStatus OrderStatus { get; set;}
         public DeliveryAddress DeliveryAddress { get; set; }
+        public List<OrderDatail> OrderDatails { get; set; }
     }
 }
